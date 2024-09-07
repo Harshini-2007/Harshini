b571a5a0fa8b9c319044cc942f39a996fc094285
Harshini Perumal <sweetharshini.007@gmail.com>
2024-09-07 19:36:39 +0530

    Add files via upload
    Printing Runner up score using python

---
+++ Runner up score python.py
@@ -0,0 +1,16 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+''' 
+a=[0]*5;
+for i in range(5):
+    a[i]=int (input()); 
+a.sort
+print("\n");
+print("runner up is :");
+print(a[2]);
+    
