5e9f787eefa4c2bc2efcb35d7e75c636288e5b98
Harshini Perumal <sweetharshini.007@gmail.com>
2024-09-20 21:18:46 +0530

    Add files via upload

---
+++ arithematic operators.py
@@ -0,0 +1,14 @@
+#**********************************************
+# python basic program
+#*********************************************
+print("This is my  1st python program to print basic arithematic operators ")
+a=10
+b=20
+sum=a+b
+print(sum)
+sub=a-b
+print(sub)
+m=a*b 
+print(m)
+d=b/a
+print(d)
