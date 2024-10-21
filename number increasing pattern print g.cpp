6998ba24ca9532bee4214622d723ff05b5341107
Harshini Perumal <sweetharshini.007@gmail.com>
2024-10-21 21:32:48 +0530

    Add files via upload

---
+++ number increasing pattern print g.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i=0,j=5,count=1;
+
+for(i=1;i<=5;i++){
+    for(j=5;j>0;j--){
+        cout<<count<<"\t";
+        count++;
+    }cout<<"\n";
+}
+}
